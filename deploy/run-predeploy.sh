ed17a37d80debebc155de04bf473d15846a2283d
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2022-12-27 11:48:46 +0100

    fix predeploy script

--- deploy/run-predeploy.sh
+++ deploy/run-predeploy.sh
@@ -3,3 +3,6 @@ cd ../react-app && MARKER=staging node ./envs/create-env.js && npm install && np
 
 # deploy staging front build:
 cd ../deploy && scp -i ./byqstaging.pem -r ./../react-app/build ubuntu@ec2-3-72-231-170.eu-central-1.compute.amazonaws.com:byq-frontend/react-app
+
+# deploy certs
+scp -i ./byqstaging.pem -r ./../cert ubuntu@ec2-3-72-231-170.eu-central-1.compute.amazonaws.com:byq-frontend
