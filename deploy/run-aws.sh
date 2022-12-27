0a7e1e7079cd9b7d9025c7cbbfd7ebaf57c99aaf
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2022-12-27 13:10:03 +0100

    fix

--- deploy/run-aws.sh
+++ deploy/run-aws.sh
@@ -6,4 +6,4 @@ sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 443 -j REDIRECT --to-p
 git checkout master && git pull
 
 # run proxy server:
-cd .. && npm install && tmux & make run-server
+cd .. && npm install && tmux npm run server
