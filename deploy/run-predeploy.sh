2e6f08e870f59105782fe68b25f649f07f3b392b
maciej.bartynski <maciej.bartynski@amsterdamstandard.com>
2022-12-28 17:42:23 +0100

    fix deploy script

--- deploy/run-predeploy.sh
+++ deploy/run-predeploy.sh
@@ -6,3 +6,6 @@ cd ../deploy && scp -i ./byqstaging.pem -r ./../react-app/build ubuntu@ec2-3-72-
 
 # deploy certs
 scp -i ./byqstaging.pem -r ./../cert ubuntu@ec2-3-72-231-170.eu-central-1.compute.amazonaws.com:byq-frontend
+
+# copy envs:
+	scp -i ./byqstaging.pem -r ../envs ubuntu@ec2-3-72-231-170.eu-central-1.compute.amazonaws.com:byq-frontend
